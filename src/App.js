90797d872ca441ca4ffd082af82f25b409039501
Sune Carelse <sune.carelse@takealot.com>
2021-02-09 22:38:22 +0200

    header rendered ith the icons and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build linkin-clone</h1>
+      <Header />
     </div>
   );
 }
