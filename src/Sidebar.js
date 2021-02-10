93f3edb7d877f988c9a420527bc7aa6080fcb2ce
Sune Carelse <sune.carelse@takealot.com>
2021-02-10 23:19:02 +0200

    sidebar created with profile and likes

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,13 +3,40 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
   return (
     <div className="sidebar">
-      <div className="siidebar__top">
-        <img src="" alt="" />
-        <Avatar />
+      <div className="sidebar__top">
+        <img src="https://cdn.wallpapersafari.com/12/63/7YgwPz.jpg" alt="" />
+        <Avatar className="sidebar__avatar" />
         <h2>Luca Van Wyk</h2>
         <h4>lucaVanWyk@gmail.com</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,400</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("design")}
+        {recentItem("blackhat-hacker")}
+        {recentItem("devops")}
       </div>
     </div>
   );
