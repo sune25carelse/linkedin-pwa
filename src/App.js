a2b5836ede3a8e54d3eedccf973bbe5a0b55993c
Sune Carelse <sune.carelse@takealot.com>
2021-02-10 23:20:37 +0200

    feed rendered

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
+import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
 
       <div className="app__body">
         <Sidebar />
+        <Feed />
       </div>
     </div>
   );
