87cb0b11769301a8fa1a9eddc4db11534526d358
Sune Carelse <sune.carelse@takealot.com>
2021-02-14 20:37:20 +0200

    login compnnt with redux functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./App.css";
+import { useSelector } from "react-redux";
+import { selecUser } from "./features/userSlice";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selecUser);
   return (
     <div className="app">
       <Header />
-
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
