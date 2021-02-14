985b314635eee3290e8228b1423bc5ec703d4c1a
Sune Carelse <sune.carelse@takealot.com>
2021-02-14 21:34:49 +0200

    stay logged in even when you refresh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import { useSelector } from "react-redux";
-import { selecUser } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selecUser } from "./features/userSlice";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Login from "./Login";
+import { auth } from "./firebase";
 
 function App() {
   const user = useSelector(selecUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
   return (
     <div className="app">
       <Header />
