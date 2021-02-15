df972777ec07552d6a25fc52ea53b34e783ea090
Sune Carelse <sune.carelse@takealot.com>
2021-02-15 21:28:26 +0200

    user info populated in fronte & letter if no image

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,11 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const user = useSelector(selectUser);
   const recentItem = (topic) => (
     <div className="sidebar__recentItem">
       <span className="sidebar__hash">#</span>
@@ -14,9 +17,11 @@ function Sidebar() {
     <div className="sidebar">
       <div className="sidebar__top">
         <img src="https://cdn.wallpapersafari.com/12/63/7YgwPz.jpg" alt="" />
-        <Avatar className="sidebar__avatar" />
-        <h2>Luca Van Wyk</h2>
-        <h4>lucaVanWyk@gmail.com</h4>
+        <Avatar src={user.photoUrl} className="sidebar__avatar">
+          {user.email[0]}
+        </Avatar>
+        <h2>{user.displayName}</h2>
+        <h4>{user.email}</h4>
       </div>
 
       <div className="sidebar__stats">
